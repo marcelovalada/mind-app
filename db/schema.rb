ba6b1bbc13ba6c041944749fa1a8c837609c25a3
Marcelo Valada <marcelovalada2@hotmail.com>
2023-05-05 17:21:49 -0300

    Add dependent destroy to tag and content

--- db/schema.rb
+++ db/schema.rb
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 2023_05_05_175919) do
   end
 
   create_table "tags", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
