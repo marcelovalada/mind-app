dac90f9fa6771d65a5cfa6808f4724e4118076a5
Marcelo Valada <marcelovalada2@hotmail.com>
2023-05-06 00:47:10 -0300

    Add option to edit content

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -8,16 +8,32 @@ class ContentsController < ApplicationController
       @content = Content.new
     end
 
+    def create
+      # @content = Content.new(content_params)
+      # @content.user = current_user
+      @content = current_user.contents.build(content_params)
+
+      if @content.save
+        redirect_to contents_path, notice: 'Content successfully created'
+      else
+        render :new
+      end
+    end
+
+    def edit
+      @content = Content.find(params[:id])
+    end
+
+    private
+
+    def content_params
+      params.require(:content).permit(:title, :description)
+    end
+
     # def show
     # end
 
-    # def create
-    # end
-
     # def update
-    # end
-
-    # def edit
     # end
 
     # def destroy
